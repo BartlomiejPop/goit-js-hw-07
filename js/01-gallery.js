4b5cf59ff4511b8f762a22d8ff88d097700ba4a4
BartlomiejPop <bartlomiejpoprawa01@gmail.com>
2023-03-29 07:59:32 +0200

    a

---
+++ js/01-gallery.js
@@ -0,0 +1,25 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryEl = document.querySelector(".gallery");
+
+galleryItems.forEach((el) => {
+	galleryEl.innerHTML += `<div class="gallery__item" >
+  <a class="gallery__link" href="${el.original}" >
+    <img
+      class="gallery__image"
+      src="${el.preview}"
+      data-source="${el.original}"
+      alt="${el.description}"
+    />
+  </a>
+</div>`;
+});
+
+galleryEl.addEventListener("click", (e) => {
+	e.preventDefault();
+	if (e.target.tagName !== "IMG") {
+		return;
+	}
+	basicLightbox.create(`<img src=${e.target.dataset.source}>`).show();
+});
